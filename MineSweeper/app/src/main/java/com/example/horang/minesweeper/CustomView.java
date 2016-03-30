2e3f9dde758632223cb37f71cc5bd8340ae5f894
David Lespine <david.lespine@epita.fr>
2016-03-30 14:04:07 +0100

    buttons

--- MineSweeper/app/src/main/java/com/example/horang/minesweeper/CustomView.java
+++ MineSweeper/app/src/main/java/com/example/horang/minesweeper/CustomView.java
@@ -1,6 +1,10 @@
 package com.example.horang.minesweeper;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Button;
